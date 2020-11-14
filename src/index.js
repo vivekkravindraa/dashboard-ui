832c96ead47ee485fd60d31fd89be33897e3cdc2
Vivek Ravindra <vivekkravindraa@Viveks-MacBook-Air.local>
2020-11-14 21:39:51 +0530

    fix: Persist redux state in localStorage

--- src/index.js
+++ src/index.js
@@ -8,7 +8,30 @@ import AppRouter from './AppRouter';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+function saveToLocalStorage(state) {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem('state', serializedState);
+  } catch(e) {
+    console.log(e);
+  }
+}
+
+function loadFromLocalStorage(state) {
+  try {
+    const serializedState = localStorage.getItem('state');
+    if(serializedState === null) return undefined;
+    return JSON.parse(serializedState);
+  } catch(e) {
+    console.log(e);
+  }
+}
+
+const persistedState = loadFromLocalStorage();
+
+const store = createStore(rootReducer, persistedState, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+store.subscribe(() => saveToLocalStorage(store.getState()));
 
 ReactDOM.render(
   <React.StrictMode>
